f34e33d59f29c6fb6949baf0fc9004f9ada829ca
julianusti <yulian.ustiyanovych@zalando.de>
2018-09-18 10:54:03 +0200

    add .eslingrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
+  },
+  env: {
+    jest: true,
+    es6: true,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        useTabs: false,
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        fluid: false,
+      },
+    ],
+  },
+};
